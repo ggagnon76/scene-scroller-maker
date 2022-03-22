a7fdbc67a3c3eb1da30e88aeeb55981861a3bdaf
ggagnon76 <ggagnon76@hotmail.com>
2022-03-22 00:22:30 -0400

    Working on Texture2Polygon

--- ssm-launch.js
+++ ssm-launch.js
@@ -1,4 +1,5 @@
 import { populateMenuButtons } from "./lib/functions.js";
+import { Texture2Polygon } from "./lib/poly_from_texture.js";
 
 // Convenience variable to insert the module name where required
 export const ModuleName = "scene-scroller-maker";
@@ -14,3 +15,7 @@ Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
 Hooks.on('getModuleToolGroups', (controlManager, toolGroup) => {
     populateMenuButtons(toolGroup);
 });
+
+Hooks.once('ready', () => {
+    game.modules.get(ModuleName).api = Texture2Polygon;
+})
