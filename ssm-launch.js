8928aa8aa8bdea408d640f479066289597652f0b
ggagnon76 <ggagnon76@hotmail.com>
2022-10-04 22:59:01 -0400

    World setting for default Compendium

--- ssm-launch.js
+++ ssm-launch.js
@@ -1,4 +1,5 @@
-import { populateMenuButtons } from "./lib/functions.js";
+import { SSM_SelectDefaultCompendium } from "./lib/forms.js";
+import { populateMenuButtons, getCompendiumPack } from "./lib/functions.js";
 import { Texture2Polygon } from "./lib/poly_from_texture.js";
 
 // Convenience variable to insert the module name where required
@@ -12,4 +13,36 @@ Hooks.on('getModuleToolGroups', (controlManager, toolGroup) => {
 
 Hooks.once('ready', () => {
     game.modules.get(ModuleName).api = Texture2Polygon;
+
+    /**
+    game.settings.registerMenu(ModuleName, "defaultImagePathMenu", {
+        name: game.i18n.localize("SSM.DefaultImagePath.Name"),
+        label: game.i18n.localize("SSM.DefaultImagePath.Label"),
+        hint: game.i18n.localize("SSM.DefaultImagePath.Hint"),
+        type: String,
+        restricted: true
+    })
+    */
+
+    game.settings.register(ModuleName, "defaultSceneCompendium", {
+        scope: "world",
+        config: false,
+        requiresReload: false,
+        type: String,
+        default: ""
+    })
+
+    const defaultCompendium = game.settings.get(ModuleName, "defaultSceneCompendium");
+    // If the compendium set as default was deleted
+    if ( !getCompendiumPack(defaultCompendium) ) {
+        game.settings.set(ModuleName, "defaultSceneCompendium", "");
+    }
+
+    game.settings.registerMenu(ModuleName, "defaultSceneCompendiumMenu", {
+        name: game.i18n.localize("SSM.DefaultSceneCompendiumMenu.Name"),
+        label: game.i18n.localize("SSM.DefaultSceneCompendiumMenu.Label"),
+        hint: game.i18n.localize("SSM.DefaultSceneCompendiumMenu.Hint"),
+        type: SSM_SelectDefaultCompendium,
+        restricted: true
+    })
 })
