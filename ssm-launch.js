7d703cbc6b149ff8aabea721f1d859b6fd8731e8
ggagnon76 <ggagnon76@hotmail.com>
2022-10-27 19:47:43 -0400

    Hide Menu Buttons and S3 bucket fixes

--- ssm-launch.js
+++ ssm-launch.js
@@ -7,11 +7,7 @@ export const ModuleName = "scene-scroller-maker";
 // Convenience variable to insert the module title where required
 export const ModuleTitle = "Scene Scroller Maker"
 
-Hooks.on('getModuleToolGroups', (controlManager, toolGroup) => {
-    populateMenuButtons(toolGroup);
-});
-
-Hooks.once('ready', () => {
+Hooks.once('init', () => {
     game.modules.get(ModuleName).api = Texture2Polygon;
 
     game.settings.register(ModuleName, "defaultImagePath", {
@@ -38,17 +34,47 @@ Hooks.once('ready', () => {
         default: ""
     })
 
-    const defaultCompendium = game.settings.get(ModuleName, "defaultSceneCompendium");
-    // If the compendium set as default was deleted
-    if ( !getCompendiumPack(defaultCompendium) ) {
-        game.settings.set(ModuleName, "defaultSceneCompendium", "");
-    }
-
     game.settings.registerMenu(ModuleName, "defaultSceneCompendiumMenu", {
         name: game.i18n.localize("SSM.DefaultSceneCompendiumMenu.Name"),
         label: game.i18n.localize("SSM.DefaultSceneCompendiumMenu.Label"),
         hint: game.i18n.localize("SSM.DefaultSceneCompendiumMenu.Hint"),
         type: SSM_SelectDefaultCompendium,
         restricted: true
+    })
+
+    game.settings.register(ModuleName, "SSM_MenuToggle", {
+        name: game.i18n.localize("SSM.MenuToggle"),
+        hint: game.i18n.localize("SSM.MenuToggleHint"),
+        scope: "client",
+        config: true,
+        requiresReload: false,
+        type: Boolean,
+        default: true,
+        onChange: () => Hooks.call('reloadModuleButtons')
+    })
+})
+
+Hooks.once('ready', () => {
+    const defaultCompendium = game.settings.get(ModuleName, "defaultSceneCompendium");
+    // If the compendium set as default was deleted
+    if ( !getCompendiumPack(defaultCompendium) ) {
+        game.settings.set(ModuleName, "defaultSceneCompendium", "");
+    }
+})
+
+Hooks.on('getModuleToolGroups', (controlManager, toolGroup) => {
+    populateMenuButtons(toolGroup);
+});
+
+Hooks.on('getSceneNavigationContext', (html, contextOptions) => {
+    contextOptions.push({
+        name: 'SSM.ToggleSSM',
+        icon: "<i class='fas fa-puzzle-piece'></i>",
+        condition: li => game.user.isGM && game.scenes.get(li.data("sceneId")).active,
+        callback: () => {
+            const setting = game.settings.get(ModuleName, "SSM_MenuToggle")
+            game.settings.set(ModuleName, "SSM_MenuToggle", !setting);
+            Hooks.call('reloadModuleButtons');
+        }
     })
 })
